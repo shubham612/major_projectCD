d4c2b744307e930d525d0ef70b073c807dac6570
shubham <shubhamrtp99@gmail.com>
2019-10-06 12:13:45 +0530

    Create user profile routers and controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// For any further routes,access from here
+// Syntax: router.use('/routerName',require('/routerFile'));
 console.log('router loaded');
 
 
