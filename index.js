700c9e71534283896116c048a3551261e5da5c3a
shubham <shubhamrtp99@gmail.com>
2019-10-06 12:24:07 +0530

    setup my view engine

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 8000;
 
 //use express router
-
 app.use('/',require('./routes'));
+
+// setup our view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 
