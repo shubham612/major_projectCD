673b296a35e780a43c35e7d390e626969e740ca8
shubham <shubhamrtp99@gmail.com>
2019-10-06 10:47:14 +0530

    add controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+// ********* module.exports.actionName = function(req,res){} ********************
+
+
+module.exports.home = function(req,res){
+    return res.end("<h1>My Codeial is running</h1>");
+}
