975e7d0dbc9f0616e481faab8f2e2589db0fdfcb
shubham <shubhamrtp99@gmail.com>
2019-10-11 22:22:09 +0530

    Sign-up for user has been created

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,8 @@
 // ********* module.exports.actionName = function(req,res){} ********************
 
-
 module.exports.home = function(req,res){
+
+    console.log(req.cookies);
     return res.render('home',{
         title: "Home"
     });
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,42 @@
+const User = require('../models/user');
 module.exports.profile = function(req,res){
    return  res.render('user_profile',{
       title: "User-Profile"
   });
+}
+
+module.exports.signUp = function(req,res){
+   return  res.render('signUp',{
+      title: "Sign-Up | Form"
+  });
+}
+
+module.exports.signIn = function(req,res){
+   return  res.render('signIn',{
+      title: "Sign-in | Form"
+  });
+}
+
+// get the sign-up data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+       return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function(err,user){
+       if(err){console.log('Error in finding user in signing up'); return; }
+
+       if(!user){
+          User.create(req.body,function(err,user){
+             if(err){console.log('Error in creating user in signing up'); return; }
+
+             return res.redirect('/users/sign-in');
+          });
+       }else{
+          return res.redirect('back');
+       }
+    });
+}
+
+module.exports.createSession = function(req,res){
+   // To- Do later
 }
